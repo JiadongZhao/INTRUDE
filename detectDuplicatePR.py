dd6e5aff0147197b8d61b28cd45ca72932be642a
Shurui Zhou <shuruiz@cs.cmu.edu>
2019-06-16 22:21:51 -0400

    throw file not exist exception

--- detectDuplicatePR.py
+++ detectDuplicatePR.py
@@ -4,16 +4,17 @@ import init
 import util.timeUtil
 import datetime
 
-# import detect
-# detect.speed_up = True
-# detect.filter_larger_number = True
-# detect.filter_out_too_old_pull_flag = True
-# detect.filter_already_cite = False
-# detect.filter_create_after_merge = True
-# detect.filter_overlap_author = False
-# detect.filter_out_too_big_pull_flag = False
-# detect.filter_same_author_and_already_mentioned = True
-# detect.filter_version_number_diff = True
+import detect
+
+detect.speed_up = True
+detect.filter_larger_number = True
+detect.filter_out_too_old_pull_flag = True
+detect.filter_already_cite = False
+detect.filter_create_after_merge = True
+detect.filter_overlap_author = False
+detect.filter_out_too_big_pull_flag = False
+detect.filter_same_author_and_already_mentioned = True
+detect.filter_version_number_diff = True
 
 add_flag = True
 
@@ -21,9 +22,7 @@ add_flag = True
 def getCandidatePRs(repo):
     candidatePR_input_file = init.PR_candidate_List_filePath_prefix + repo.replace('/', '.') + '.txt'
     print(candidatePR_input_file)
-    with open(candidatePR_input_file, 'a') as f:  # opens file for appending
-            print("", file=f) 
-    
+
     has = set()
     prCandidate_list = []
     if os.path.exists(candidatePR_input_file):
@@ -37,8 +36,8 @@ def getCandidatePRs(repo):
     # get all pr
     pull_list = get_repo_info(repo, 'pull')  # get all info about all PRs, sort by ID
     pull_list = sorted(pull_list, key=lambda x: int(x['number']), reverse=True)
-    print("length : "+ str(len(pull_list)))
-    
+    print("length : " + str(len(pull_list)))
+
     for current_pr in pull_list:  # iterate through PRs
         current_pr_id = current_pr['number']
         current_pr_createdAt = current_pr['created_at']
@@ -57,7 +56,7 @@ def getCandidatePRs(repo):
 
         prCandidate_list.append((repo, str(current_pr_id)))
 
-    print("length of pr candidates: "+str(len(prCandidate_list)))
+    print("length of pr candidates: " + str(len(prCandidate_list)))
     for pair in prCandidate_list:
         with open(candidatePR_input_file, 'a') as f:  # opens file for appending
             print("\t".join(pair), file=f)  # print pairs, separated by tabs, and followed by filename
@@ -72,14 +71,19 @@ def work():
         getCandidatePRs(repo)
         candidatePR_input_file = init.PR_candidate_List_filePath_prefix + repo.replace('/', '.') + '.txt'
 
-        with open(candidatePR_input_file) as f:
-            for t in f.readlines():
-                repo, pr_id = t.split()
-                cnt += 1
-#                 dupPR_id, similarity = detect.detect_one(repo, pr_id)
+        try:
+            with open(candidatePR_input_file) as f:
+                for t in f.readlines():
+                    repo, pr_id = t.split()
+                    cnt += 1
+        except FileNotFoundError:
+            print("file not exist, continue")
+            continue
 
-#                 with open(init.dupPR_result_filePath_prefix + repo.replace('/', '.') + '.txt', 'a') as outf:
-#                     print(repo, pr_id, dupPR_id, similarity, sep='\t', file=outf)
+        dupPR_id, similarity = detect.detect_one(repo, pr_id)
+
+        with open(init.dupPR_result_filePath_prefix + repo.replace('/', '.') + '.txt', 'a') as outf:
+                print(repo, pr_id, dupPR_id, similarity, sep='\t', file=outf)
 
 
 if __name__ == "__main__":
