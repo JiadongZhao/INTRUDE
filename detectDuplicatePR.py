f064f63e07de45363b398002d60bafb5447dc412
Shurui Zhou <shuruiz@cs.cmu.edu>
2019-07-30 14:40:53 -0400

    change time

--- detectDuplicatePR.py
+++ detectDuplicatePR.py
@@ -128,7 +128,7 @@ def exeEveryDay():
     today_str = str(x).split(" ")[0]+"_test"
     print('today : ' + today_str)
 
-    y = x.replace(day=x.day + 1, hour=12, minute=0, second=0, microsecond=0)
+    y = x.replace(day=x.day + 1, hour=15, minute=0, second=0, microsecond=0)
 
     delta_t = y - x
     secs = delta_t.seconds + 1
