f537ab1381c9d05993ec74b3cc61409512418807
Shurui Zhou <shuruiz@cs.cmu.edu>
2019-07-30 11:22:29 -0400

    test everyday exec

--- detectDuplicatePR.py
+++ detectDuplicatePR.py
@@ -125,10 +125,10 @@ today = ''
 
 def exeEveryDay():
     x = datetime.today()
-    today_str = str(x).split(" ")[0]
+    today_str = str(x).split(" ")[0]+"_test"
     print('today : ' + today_str)
 
-    y = x.replace(day=x.day + 1, hour=15, minute=0, second=0, microsecond=0)
+    y = x.replace(day=x.day + 1, hour=12, minute=0, second=0, microsecond=0)
 
     delta_t = y - x
     secs = delta_t.seconds + 1
